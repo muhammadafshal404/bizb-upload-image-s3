961d2d374525e1e5a0b22884eb99f343d9ecd706
husamaz1994 <11besehzulfiqar@seecs.edu.pk>
2021-09-20 17:14:33 +0500

    Product Info done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,66 @@
 import pkg from "../package.json";
+import SimpleSchema from "simpl-schema";
+import importAsString from "@reactioncommerce/api-utils/importAsString.js";
+const mySchema = importAsString("./schema.graphql");
+import getVariantsByUserId from "./utils/getVariants.js";
+
+var _context=null;
+const resolvers = {
+  Account: {
+     async productVariants(parent, args, context, info) {
+       console.log("parent",parent);
+      let productVariant=await getVariantsByUserId(context, parent.userId, true, args);
+      return productVariant;
+    }
+  }
+};
+function myStartup(context) {
+ _context=context;
+  const OwnerInfo = new SimpleSchema({
+    userId: {
+      type: String,
+      max: 30,
+      optional: true,
+    },
+    image: {
+      type: String,
+      max: 20,
+      optional: true,
+    },
+    name: {
+      type: String,
+      optional: true,
+    },
+  });
+
+  context.simpleSchemas.ProductVariant.extend({
+    uploadedBy: OwnerInfo,
+    isPublished:Boolean
+  });
+
+  context.simpleSchemas.CatalogProductVariant.extend({
+    uploadedBy: OwnerInfo,
+    isPublished:Boolean
+
+  });
+}
+
+// The new myPublishProductToCatalog function parses our products,
+// gets the new uploadedBy attribute, and adds it to the corresponding catalog variant in preparation for publishing it to the catalog
+function myPublishProductToCatalog(
+  catalogProduct,
+  { context, product, shop, variants }
+) {
+
+  catalogProduct.variants &&
+    catalogProduct.variants.map((catalogVariant) => {
+      const productVariant = variants.find(
+        (variant) => variant._id === catalogVariant.variantId
+      );
+      catalogVariant.uploadedBy = productVariant.uploadedBy || null;
+      catalogVariant.isPublished = productVariant.isPublished || null;
+    });
+}
 
 /**
  * @summary Import and call this function to add this plugin to your API.
@@ -7,8 +69,16 @@ import pkg from "../package.json";
  */
 export default async function register(app) {
   await app.registerPlugin({
-    label: "Plugin Example",
-    name: "plugin-example",
-    version: pkg.version
+    label: "Product Dimensions",
+    name: "products-dimensions",
+    version: pkg.version,
+    functionsByType: {
+      startup: [myStartup],
+      publishProductToCatalog: [myPublishProductToCatalog],
+    },
+    graphQL: {
+      schemas: [mySchema],
+      resolvers
+    },
   });
 }
